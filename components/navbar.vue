349bf28a90bf22d683ae78f4cc54e42d8f57a781
matiss <matissjurevics@gmail.com>
2022-06-17 12:05:41 +0100

    set up project and added basic components

---
+++ components/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar bg-base-100 px-8">
+    <div class="flex-1">
+      <a class="normal-case text-xl font-semibold">UI Generator</a>
+    </div>
+    <div class="flex-none gap-2">
+      <div class="form-control">
+        <input type="text" placeholder="Search" class="input input-bordered" />
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="h-screen w-screen bg-slate-800">
+    <Navbar />
+    <section class="p-8">
+      <h1
+        class="w-fit text-4xl bg-gradient-to-br from-blue-400 to-green-400 bg-clip-text text-transparent font-bold"
+      >
+        Generate UI Components
+      </h1>
+      <div class="w-full h-[2px] bg-white/30 my-2"/>
+
+      <div class="mt-6">
+        <div class="card w-96 bg-base-100 drop-shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
